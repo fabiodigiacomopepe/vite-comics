f8279e54ee656bad95cb76c399540dd2f63cab07
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-06-01 15:39:04 +0200

    Add AppJumbotron and transfer data from AppHeader to App (use props)

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,60 +3,11 @@ export default {
     name: "AppHeader",
     data() {
         return {
-            percorsoLogo: "./src/assets/img/dc-logo.png",
-            sezioniNav: [
-                {
-                    text: "CHARACTERS",
-                    url: "#",
-                    current: false
-                },
-                {
-                    text: "COMICS",
-                    url: "#",
-                    current: true
-                },
-                {
-                    text: "MOVIES",
-                    url: "#",
-                    current: false
-                },
-                {
-                    text: "TV",
-                    url: "#",
-                    current: false
-                },
-                {
-                    text: "GAMES",
-                    url: "#",
-                    current: false
-                },
-                {
-                    text: "COLLECTIBLES",
-                    url: "#",
-                    current: false
-                },
-                {
-                    text: "VIDEOS",
-                    url: "#",
-                    current: false
-                },
-                {
-                    text: "FANS",
-                    url: "#",
-                    current: false
-                },
-                {
-                    text: "NEWS",
-                    url: "#",
-                    current: false
-                },
-                {
-                    text: "SHOP",
-                    url: "#",
-                    current: false
-                },
-            ]
+            percorsoLogo: "./src/assets/img/dc-logo.png"
         }
+    },
+    props: {
+        sezioniNav: Array
     }
 }
 </script>
---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: "AppJumbotron",
+}
+</script>
+
+<template>
+    <section>
+        <img src="../assets/img/jumbotron.jpg" alt="jumbtron">
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables.scss' as *;
+@use '../styles/partials/mixins.scss' as *;
+
+section {
+    height: 320px;
+}
+
+img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: top;
+}
+</style>
